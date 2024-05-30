2b00a37cf88b0b47738d89c11c3c5471aad1d9e9
Brendon O'Laney <brendon@brendonolaney.com>
2024-05-30 11:16:33 -0700

    Update wezterm font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'CommitMono'
+config.font = wezterm.font 'CommitMono Nerd Font'
 config.font_size = 14
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
