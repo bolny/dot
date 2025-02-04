4544103f21d1cffbc9a449389aeb42e80bff8cbc
Brendon O'Laney <brendon@brendonolaney.com>
2025-02-04 13:19:35 -0800

    remove automatic device setup and turn it into a guide

--- .share/mac_init.zsh
+++ .share/mac_init.zsh
@@ -1,51 +1,41 @@
 #! /bin/zsh
 
-source ./_mac_init_funcs.zsh
-
 echo "Initializing new computer."
 echo "==="
 
 echo "\n\nInstall Homebrew"
-homebrew_preview
-pause_until_enter
-homebrew_exec
+echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 
 echo "\n\nGenerate ssh keypair for github"
-keypair_gen_preview
-pause_until_enter
-keypair_gen_exec
+echo "ssh-keygen"
 
 echo "\n\nInstall ssh public key in github"
-install_github_preview
-pause_until_enter
-install_github_exec
+echo "open github, sign in and add the contents of id_ed25519.pub to your ssh keys"
+echo "cat ~/.ssh/id_ed25519.pub | pbcopy"
 
 echo "\n\nTODO: set up ~/.ssh/config"
-ssh_config_preview
-pause_until_enter
-ssh_config_exec
+echo "Add the following to ~/.ssh/config"
+echo <<- EOF
+Host *
+    UseKeychain yes
+    AddKeysToAgent yes
+    IdentityFile ~/.ssh/id_rsa
+EOF
+
 
 echo "\n\nCreate source folders"
-folders_preview
-pause_until_enter
-folders_exec
+echo "mkdir -p ~/Source/github.com/bolny"
 
 echo "\n\nSet up dot files"
-clone_preview
-pause_until_enter
-clone_exec
+echo 'git clone --bare git@github.com:bolny/dot.git $HOME/Source/github.com/bolny/dot.git'
+echo 'git --git-dir=$HOME/Source/github.com/bolny/dot.git/ --work-tree=$HOME reset --hard HEAD'
+echo 'git --git-dir=$HOME/Source/github.com/bolny/dot.git/ --work-tree=$HOME config --local status.showUntrackedFiles no'
 
 echo "\n\nSource zshrc and zprofile."
-source_preview
-pause_until_enter
-source_exec
+echo 'source ~/.zshrc; source ~/.zprofile'
 
 echo "\n\nInstall vim plugins."
-vim_preview
-pause_until_enter
-vim_exec
+echo 'vim -c "PlugInstall|qa!"'
 
 echo "\n\nInstall homebrew packages."
-homebrew_install_preview
-pause_until_enter
-homebrew_install_exec
+echo 'brew bundle --global'
