79f17146a0c82e0e7a80d8b7383fc645b1c6dfed
Brendon O'Laney <brendon@brendonolaney.com>
2025-02-04 13:36:36 -0800

    update device init script

--- .share/mac_init.sh
+++ .share/mac_init.sh
@@ -1,21 +1,23 @@
-#! /bin/zsh
+#! /bin/sh
+# This file is intended to provide text ready to copy and paste
+# shellcheck disable=2016
 
 echo "Initializing new computer."
 echo "==="
 
-echo "\n\nInstall Homebrew"
+printf "\n\nInstall Homebrew"
 echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 
-echo "\n\nGenerate ssh keypair for github"
+printf "\n\nGenerate ssh keypair for github"
 echo "ssh-keygen"
 
-echo "\n\nInstall ssh public key in github"
+printf "\n\nInstall ssh public key in github"
 echo "open github, sign in and add the contents of id_ed25519.pub to your ssh keys"
 echo "cat ~/.ssh/id_ed25519.pub | pbcopy"
 
-echo "\n\nTODO: set up ~/.ssh/config"
+printf "\n\nTODO: set up ~/.ssh/config"
 echo "Add the following to ~/.ssh/config"
-echo <<- EOF
+cat <<- EOF
 Host *
     UseKeychain yes
     AddKeysToAgent yes
@@ -23,19 +25,19 @@ Host *
 EOF
 
 
-echo "\n\nCreate source folders"
+printf "\n\nCreate source folders"
 echo "mkdir -p ~/Source/github.com/bolny"
 
-echo "\n\nSet up dot files"
+printf "\n\nSet up dot files"
 echo 'git clone --bare git@github.com:bolny/dot.git $HOME/Source/github.com/bolny/dot.git'
 echo 'git --git-dir=$HOME/Source/github.com/bolny/dot.git/ --work-tree=$HOME reset --hard HEAD'
 echo 'git --git-dir=$HOME/Source/github.com/bolny/dot.git/ --work-tree=$HOME config --local status.showUntrackedFiles no'
 
-echo "\n\nSource zshrc and zprofile."
+printf "\n\nSource zshrc and zprofile."
 echo 'source ~/.zshrc; source ~/.zprofile'
 
-echo "\n\nInstall vim plugins."
+printf "\n\nInstall vim plugins."
 echo 'vim -c "PlugInstall|qa!"'
 
-echo "\n\nInstall homebrew packages."
+printf "\n\nInstall homebrew packages."
 echo 'brew bundle --global'
