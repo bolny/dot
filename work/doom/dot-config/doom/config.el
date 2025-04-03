535c811c36a7c94c36c818c75be569cd3227f67e
Brendon O'Laney <brendon@brendonolaney.com>
2025-04-03 11:01:15 -0700

    Update YAML support for doom

--- work/doom/dot-config/doom/config.el
+++ work/doom/dot-config/doom/config.el
@@ -81,3 +81,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; set yaml mode for yml files.
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
