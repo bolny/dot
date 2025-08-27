306a79a4c2abf2872e5ddc8e90ae289c1a0fa8cd
Brendon O'Laney <brendon@brendonolaney.com>
2025-08-27 14:46:54 -0700

    updates

---
+++ work/wezterm/dot-config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Solarized Light (Gogh)'
+config.enable_tab_bar = false
+config.font = wezterm.font 'Drafting*Mono Nerd Font'
+config.font_size = 14.0
+config.default_prog = { '/opt/homebrew/bin/fish', '-l', '-C', 'fish_vi_key_bindings'}
+
+return config
