d74855e5f799234a07f566a95fad0b73a5541808
Brendon O'Laney <brendon@brendonolaney.com>
2025-11-25 07:54:32 -0800

    remove the last vestigaes of vi mode

--- work/wezterm/dot-config/wezterm/wezterm.lua
+++ work/wezterm/dot-config/wezterm/wezterm.lua
@@ -5,6 +5,6 @@ config.color_scheme = 'Solarized Light (Gogh)'
 config.enable_tab_bar = false
 config.font = wezterm.font 'Drafting*Mono Nerd Font'
 config.font_size = 14.0
-config.default_prog = { '/opt/homebrew/bin/fish', '-l', '-C', 'fish_vi_key_bindings'}
+config.default_prog = { '/opt/homebrew/bin/fish', '-l'}
 
 return config
