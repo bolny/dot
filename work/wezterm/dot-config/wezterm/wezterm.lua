92bb67ca8d1049c86349b6b7fbed1d2dc690a2eb
Brendon O'Laney <brendon@brendonolaney.com>
2025-11-25 10:16:39 -0800

    add tabs to wezterm

--- work/wezterm/dot-config/wezterm/wezterm.lua
+++ work/wezterm/dot-config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'Solarized Light (Gogh)'
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
 config.font = wezterm.font 'Drafting*Mono Nerd Font'
 config.font_size = 14.0
 config.default_prog = { '/opt/homebrew/bin/fish', '-l'}
