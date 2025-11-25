fb79e296132ab1d9a0416fa596fe2067d3b2bf0b
Brendon O'Laney <brendon@brendonolaney.com>
2025-11-25 12:33:47 -0800

    massive overhaul of work

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Brendon's dotfiles
 
-## Quickstart
+The dot files are managed by GNU stow. To use it do the following:
 
+To link a dot files, cd to `home` or `work` and run:
+
+```bash
+stow --verbose --dotfiles --target="$HOME" "<name of package>"
 ```
-git init --bare $HOME/Source/github.com/bolny/dot/
-alias config='git --git-dir=$HOME/Source/github.com/bolny/dot/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
+
+where the "name of package" is the subdir of the app to link. For example:
+
+```bash
+stow --dotfiles -v -t "$HOME" "fish"
 ```
+
+You can delete the existing links by adding `-D` or `--delete` or overwrite them
+with `-R` or `--restow`. The `-D` or `--dir` argument is handy if you want to
+use this outside of the `work` or `home` directory
--- work/fish/dot-config/fish/config.fish
+++ work/fish/dot-config/fish/config.fish
@@ -1,99 +1,126 @@
 if status is-interactive
-  # Homebrew setup
-  set -x PATH /opt/homebrew/bin $PATH
-  set -x PATH /opt/homebrew/sbin $PATH
+    # Homebrew setup
+    set -x PATH /opt/homebrew/bin $PATH
+    set -x PATH /opt/homebrew/sbin $PATH
+    set -x HOMEBREW_BUNDLE_FILE_GLOBAL "$HOME"/.config/homebrew/Brewfile
 
-  set -x HOMEBREW_BUNDLE_FILE_GLOBAL "$HOME"/.config/homebrew/Brewfile
-
-  # Completions
-  if test -d (brew --prefix)"/share/fish/completions"
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
-  end
-
-  # Package completions
-  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-  end
-
-  # FZF integration
-  if type -q fzf
-    fzf --fish | source
-  end
-
-  # Set up starship prompt
-  if type -q starship
-    starship init fish | source
-  end
-
-  # Set up ls colorscheme
-  if type -q vivid
-    set -x LS_COLORS (vivid generate solarized-light)
-  end
-
-  # Set up bat theme to provide syntax highlighting to terminal programs.
-  set -x BAT_THEME "Solarized (light)"
-
-  if type -q zoxide
-    zoxide init fish | source
-    alias cd="z"            # Use zoxide instead of cd
-  end
-
-  if type -q eza
-    alias ls="eza"          # Use eza instead of ls
-    alias tree="eza --tree" # Use eza instead of tree
-  end
-
-  if type -q rg
-    alias grep="rg"        # Use rg instead of grep
-  end
-
-  if type -q fd
-    alias find="fd"        # Use fd instead of find
-  end
-
-  if type -q bat
-    alias cat="bat"        # Use bat instead of cat
-  end
-
-  if type -q delta
-    alias diff="delta"     # use delta instead of diff
-  end
-
-  if type -q trash
-    function rm
-      if test "$argv[1]" = "-rf" -o "$argv[1]" = "-fr"
-        # Handle rm -rf by calling trash instead
-        trash $argv[2..-1]
-      else
-        # Handle other rm commands by calling trash
-        trash $argv
-      end
+    # Use homebrew curl
+    if test -d (brew --prefix)"/opt/curl/bin"
+        set -x PATH (brew --prefix)/opt/curl/bin $PATH
+    else
+        echo "Homebrew curl not available."
     end
-  end
 
-  # useful aliases
-  alias l="eza"           # List files
-  alias t="eza --tree"    # list files as Tree
-  alias w="lynx -vikeys"  # Web browse
-  alias q="duck"          # Query search engine
-  alias g="git"           # source Gontrol
-  alias s="rg"            # Search inside files
-  alias f="fd"            # Find files
-  alias c="bat"           # Cat file
-  alias v="vim"           # View file in editor
-  alias d="delta"         # Diff files
-  alias r="trash"         # Removes file
+    # Completions
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    else
+        echo "Fish completetions not available."
+    end
 
-  # Global python provided by uv
-  # Implement when https://github.com/astral-sh/uv/issues/7377 is resolved.
+    # Package completions
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    else
+        echo "Homebrew package completions for fish not available."
+    end
 
-  # Global node provided by volta
-  set -x PATH "$HOME"/.volta/bin $PATH
+    # FZF integration
+    if type -q fzf
+        fzf --fish | source
+    else
+        echo "FZF not available."
+    end
 
-  # Doom bins
-  set -x PATH "$HOME"/.config/emacs/bin $PATH
+    # Set up starship prompt
+    if type -q starship
+        starship init fish | source
+    else
+        echo "Starship not available."
+    end
 
-  # Personal bins
-  set -x PATH "$HOME"/.bin $PATH
-  set -x PATH "$HOME"/.local/bin $PATH
+    # Set up ls colorscheme
+    if type -q vivid
+        set -x LS_COLORS (vivid generate solarized-light)
+    else
+        echo "Vivid not available."
+    end
+
+    # Set up bat theme to provide syntax highlighting to terminal programs.
+    set -x BAT_THEME "Solarized (light)"
+
+    # Set up ergonomic alternatives to common commands
+    if type -q zoxide
+        zoxide init fish | source
+        alias cd="z" # Use zoxide instead of cd
+    else
+        echo "Zoxide not available."
+    end
+
+    if type -q eza
+        alias ls="eza" # Use eza instead of ls
+        alias tree="eza --tree" # Use eza instead of tree
+    else
+        echo "Eza not available."
+    end
+
+    if type -q rg
+        alias grep="rg" # Use rg instead of grep
+    else
+        echo "Ripgrep not available."
+    end
+
+    if type -q fd
+        alias find="fd" # Use fd instead of find
+    else
+        echo "Fd not available."
+    end
+
+    if type -q bat
+        alias cat="bat" # Use bat instead of cat
+    else
+        echo "Bat not available."
+    end
+
+    if type -q delta
+        alias diff="delta" # use delta instead of diff
+    else
+        echo "Delta not available."
+    end
+
+    if type -q trash
+        function rm
+            if test "$argv[1]" = -rf -o "$argv[1]" = -fr
+                # Handle rm -rf by calling trash instead
+                trash $argv[2..-1]
+            else
+                # Handle other rm commands by calling trash
+                trash $argv
+            end
+        end
+    else
+        echo "Trash not available."
+    end
+
+    if type -q hx
+        set -x EDITOR hx
+    else
+        echo "Helix not available."
+    end
+
+    # useful aliases
+    alias l="eza" # List files
+    alias t="eza --tree" # list files as Tree
+    alias w="chawan" # Web browse
+    alias g="git" # source Gontrol
+    alias s="rg" # Search inside files
+    alias f="fd" # Find files
+    alias c="bat" # Cat file
+    alias v="hx" # View file in editor
+    alias d="delta" # Diff files
+    alias r="trash" # Removes file
+
+    # Personal bins
+    set -x PATH "$HOME"/.bin $PATH
+    set -x PATH "$HOME"/.local/bin $PATH
 end
