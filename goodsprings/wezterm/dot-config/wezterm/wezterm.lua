ad568c3ea8b3621d3846052987ebefa7e8c9b116
Brendon O'Laney <brendon@brendonolaney.com>
2025-11-27 09:39:37 -0800

    Add docker lsps

--- goodsprings/wezterm/dot-config/wezterm/wezterm.lua
+++ goodsprings/wezterm/dot-config/wezterm/wezterm.lua
@@ -1,15 +1,25 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.gui.get_appearance():find("Dark") then
-  config.color_scheme = 'Solarized Dark (Gogh)'
+    config.color_scheme = "Solarized Dark (Gogh)"
 else
-  config.color_scheme = 'Solarized Light (Gogh)'
+    config.color_scheme = "Solarized Light (Gogh)"
 end
 
-config.window_decorations = 'INTEGRATED_BUTTONS'
-config.font = wezterm.font({ family= 'Drafting*Mono Nerd Font' })
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "INTEGRATED_BUTTONS"
+config.font = wezterm.font({ family = "Drafting*Mono Nerd Font" })
 config.font_size = 14
-config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+
+config.keys = {
+    {
+        key = "p",
+        mods = "SUPER",
+        action = wezterm.action.PaneSelect,
+    },
+}
 
 return config
