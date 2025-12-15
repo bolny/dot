448d3981a08c504089c7888e6e0690ca9dce3b53
Brendon O'Laney <brendon@brendonolaney.com>
2025-12-15 15:22:49 -0800

    add secrets

--- goodsprings/fish/dot-config/fish/config.fish
+++ goodsprings/fish/dot-config/fish/config.fish
@@ -131,4 +131,9 @@ if status is-interactive
     # Personal bins
     set -x PATH "$HOME"/.bin $PATH
     set -x PATH "$HOME"/.local/bin $PATH
+
+    if test -e "$HOME/secrets.fish"
+        source "$HOME/secrets.fish"
+    end
+
 end
